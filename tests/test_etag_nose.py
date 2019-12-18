5a049b5cbfca3fd0c866222d8330d64617a5828e
SandySalvatore <ts-lankishetty.sande@rakuten.com>
2019-12-18 10:16:28 +0530

    1.4.5.post2

--- tests/test_etag_nose.py
+++ tests/test_etag_nose.py
@@ -1,11 +1,10 @@
-import pytest
-
 from webob.etag import IfRange, ETagMatcher
 from webob import Response
+from nose.tools import eq_, assert_raises
 
 def test_if_range_None():
     ir = IfRange.parse(None)
-    assert str(ir) == ''
+    eq_(str(ir), '')
     assert not ir
     assert Response() in ir
     assert Response(etag='foo') in ir
@@ -14,7 +13,7 @@ def test_if_range_None():
 def test_if_range_match_date():
     date = 'Fri, 09 Nov 2001 01:08:47 GMT'
     ir = IfRange.parse(date)
-    assert str(ir) == date
+    eq_(str(ir), date)
     assert Response() not in ir
     assert Response(etag='etag') not in ir
     assert Response(etag=date) not in ir
@@ -23,7 +22,7 @@ def test_if_range_match_date():
 
 def test_if_range_match_etag():
     ir = IfRange.parse('ETAG')
-    assert str(ir) == '"ETAG"'
+    eq_(str(ir), '"ETAG"')
     assert Response() not in ir
     assert Response(etag='other') not in ir
     assert Response(etag='ETAG') in ir
@@ -31,19 +30,19 @@ def test_if_range_match_etag():
 
 def test_if_range_match_etag_weak():
     ir = IfRange.parse('W/"ETAG"')
-    assert str(ir) == ''
+    eq_(str(ir), '')
     assert Response(etag='ETAG') not in ir
     assert Response(etag='W/"ETAG"') not in ir
 
 def test_if_range_repr():
-    assert repr(IfRange.parse(None)) == 'IfRange(<ETag *>)'
-    assert str(IfRange.parse(None)) == ''
+    eq_(repr(IfRange.parse(None)), 'IfRange(<ETag *>)')
+    eq_(str(IfRange.parse(None)), '')
 
 def test_resp_etag():
     def t(tag, res, raw, strong):
-        assert Response(etag=tag).etag == res
-        assert Response(etag=tag).headers.get('etag') == raw
-        assert Response(etag=tag).etag_strong == strong
+        eq_(Response(etag=tag).etag, res)
+        eq_(Response(etag=tag).headers.get('etag'), raw)
+        eq_(Response(etag=tag).etag_strong, strong)
     t('foo', 'foo', '"foo"', 'foo')
     t('"foo"', 'foo', '"foo"', 'foo')
     t('a"b', 'a"b', '"a\\"b"', 'a"b')
@@ -58,13 +57,14 @@ def test_resp_etag():
 def test_matcher():
     matcher = ETagMatcher(['ETAGS'])
     matcher = ETagMatcher(['ETAGS'])
-    assert matcher.etags == ['ETAGS']
+    eq_(matcher.etags, ['ETAGS'])
+    assert_raises(DeprecationWarning, matcher.weak_match, "etag")
     assert "ETAGS" in matcher
     assert "WEAK" not in matcher
     assert "BEER" not in matcher
     assert None not in matcher
-    assert repr(matcher) == '<ETag ETAGS>'
-    assert str(matcher) == '"ETAGS"'
+    eq_(repr(matcher), '<ETag ETAGS>')
+    eq_(str(matcher), '"ETAGS"')
 
     matcher2 = ETagMatcher(("ETAG1","ETAG2"))
-    assert repr(matcher2) == '<ETag ETAG1 or ETAG2>'
+    eq_(repr(matcher2), '<ETag ETAG1 or ETAG2>')
--- tests/test_multidict.py
+++ tests/test_multidict.py
@@ -308,14 +308,12 @@ class NestedMultiDictTestCase(BaseDictTests, unittest.TestCase):
 class TestGetDict(BaseDictTests, unittest.TestCase):
     klass = multidict.GetDict
 
-    def _get_instance(self, environ=None, **kwargs):
-        if environ is None:
-            environ = {}
+    def _get_instance(self, **kwargs):
         if kwargs:
             data = multidict.MultiDict(kwargs)
         else:
             data = self.data.copy()
-        return self.klass(data, environ)
+        return self.klass(data, {})
 
     def test_inititems(self):
         #The first argument passed into the __init__ method
@@ -334,92 +332,51 @@ class TestGetDict(BaseDictTests, unittest.TestCase):
         d.extend(test = 'a')
         self.assertEqual(d['test'], 'a')
 
-    def test_extend_from_items(self):
-        values = {'a': '1', 'b': '2', 'c': '3'}
-        class MappingWithItems:
+    def test_listextend(self):
+        class Other:
             def items(self):
-                return values.items()
+                return [text_('\xe9'), 'e', 'f', 1]
 
+        other = Other()
         d = self._get_instance()
-        d.extend(MappingWithItems())
-        self.assertTrue(set(values.items()).issubset(d._items))
+        d.extend(other)
 
-    def test_extend_from_keys(self):
-        values = {'a': '1', 'b': '2', 'c': '3'}
-        class MappingWithoutItems:
+        _list = [text_('\xe9'), 'e', r'f', 1]
+        for v in _list:
+            self.assertTrue(v in d._items)
+
+    def test_dictextend(self):
+        class Other:
             def __getitem__(self, item):
-                return values[item]
+                return {'a':1, 'b':2, 'c':3}.get(item)
+
             def keys(self):
-                return values.keys()
+                return ['a', 'b', 'c']
 
+        other = Other()
         d = self._get_instance()
-        d.extend(MappingWithoutItems())
-        self.assertTrue(set(values.items()).issubset(d._items))
+        d.extend(other)
 
-    def test_extend_from_iterable(self):
-        items = [('a', '1')]
+        _list = [('a', 1), ('b', 2), ('c', 3)]
+        for v in _list:
+            self.assertTrue(v in d._items)
+
+    def test_otherextend(self):
+        class Other(object):
+            def __iter__(self):
+                return iter([('a', 1)])
+
+        other = Other()
         d = self._get_instance()
+        d.extend(other)
 
-        d.extend(iter(items))
-        self.assertTrue(set(items).issubset(d._items))
+        _list = [('a', 1)]
+        for v in _list:
+            self.assertTrue(v in d._items)
 
     def test_repr_with_password(self):
         d = self._get_instance(password='pwd')
         self.assertEqual(repr(d), "GET([('password', '******')])")
-
-    def test_setitem_updates_QUERY_STRING(self):
-        env = {}
-        d = self._get_instance(environ=env)
-        d['a'] = '2'
-        self.assertEqual(env['QUERY_STRING'], 'b=1&a=2')
-
-    def test_add_updates_QUERY_STRING(self):
-        env = {}
-        d = self._get_instance(environ=env)
-        d.add('a', '2')
-        self.assertEqual(env['QUERY_STRING'], 'a=%C3%A9&a=e&a=f&b=1&a=2')
-
-    def test_delitem_updates_QUERY_STRING(self):
-        env = {}
-        d = self._get_instance(environ=env)
-        del d['a']
-        self.assertEqual(env['QUERY_STRING'], 'b=1')
-
-    def test_clear_updates_QUERY_STRING(self):
-        env = {}
-        d = self._get_instance(environ=env)
-        d.clear()
-        self.assertEqual(env['QUERY_STRING'], '')
-
-    def test_setdefault_updates_QUERY_STRING(self):
-        env = {}
-        d = self._get_instance(environ=env)
-        d.setdefault('c', '2')
-        self.assertEqual(env['QUERY_STRING'], 'a=%C3%A9&a=e&a=f&b=1&c=2')
-
-    def test_pop_updates_QUERY_STRING(self):
-        env = {}
-        d = self._get_instance(environ=env)
-        d.pop('a')
-        self.assertEqual(env['QUERY_STRING'], 'a=e&a=f&b=1')
-
-    def test_popitem_updates_QUERY_STRING(self):
-        env = {}
-        d = self._get_instance(environ=env)
-        d.popitem()
-        self.assertEqual(env['QUERY_STRING'], 'a=%C3%A9&a=e&a=f')
-
-    def test_update_updates_QUERY_STRING(self):
-        env = {}
-        d = self._get_instance(environ=env)
-        d.update([('a', '2')])
-        self.assertEqual(env['QUERY_STRING'], 'b=1&a=2')
-
-    def test_extend_updates_QUERY_STRING(self):
-        env = {}
-        d = self._get_instance(environ=env)
-        d.extend([('a', '2')])
-        self.assertEqual(env['QUERY_STRING'], 'a=%C3%A9&a=e&a=f&b=1&a=2')
 
 class NoVarsTestCase(unittest.TestCase):
     klass = multidict.NoVars
